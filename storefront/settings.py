285016c74b281b04be5e7489742d141cee283ed1
Mehfooz <graphicx.mehfooz201@gmail.com>
2023-10-15 22:24:18 +0500

    installing third party library djoser,  simplejwt

--- storefront/settings.py
+++ storefront/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'django_filters',
+
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -149,6 +151,13 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS' : 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE' : 10,
 
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+   'AUTH_HEADER_TYPES': ('JWT',),
 }
 
 AUTH_USER_MODEL = 'core.User'
